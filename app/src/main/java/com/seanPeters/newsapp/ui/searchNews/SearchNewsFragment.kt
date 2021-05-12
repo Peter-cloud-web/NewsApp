1e0ed9040e0f55eba2c609920758fa128d62896b
Peter-cloud-web <petergichia165@gmail.com>
2021-05-12 05:17:53 +0300

    create api package to hold api data classess and responses

--- app/src/main/java/com/seanPeters/newsapp/ui/searchNews/SearchNewsFragment.kt
+++ app/src/main/java/com/seanPeters/newsapp/ui/searchNews/SearchNewsFragment.kt
@@ -9,10 +9,7 @@ import com.seanPeters.newsapp.R
 
 class SearchNewsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_news, container, false)
     }
