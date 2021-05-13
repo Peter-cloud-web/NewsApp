4c5229da3c52df2a8ec6da9f299862a362a8d1b5
Peter-cloud-web <petergichia165@gmail.com>
2021-05-13 05:47:38 +0300

    create NewsArticleDao and define sqlite statements to query and transact the database

--- app/src/main/java/com/seanPeters/newsapp/data/NewsArticleDao.kt
+++ app/src/main/java/com/seanPeters/newsapp/data/NewsArticleDao.kt
@@ -1,4 +1,26 @@
 package com.seanPeters.newsapp.data
 
-class NewsArticleDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+
+@Dao
+interface NewsArticleDao {
+
+    @Query("SELECT * FROM breaking_news INNER JOIN news_articles ON articleUrl = url")
+    fun getAllBreakingNews(): Flow<List<NewsArticle>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArticles(articles:List<NewsArticle>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBreakingNews(breakingNews:List<BreakingNews>)
+
+    @Query("DELETE FROM breaking_news")
+    suspend fun deleteAllBreakingNews()
+
 }
