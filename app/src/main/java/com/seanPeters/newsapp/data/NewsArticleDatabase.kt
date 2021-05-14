ad1a8ee0c799c861c9e5fca2a61e98bc5bc23aca
Peter-cloud-web <petergichia165@gmail.com>
2021-05-14 04:07:18 +0300

    Add dependency injection to inject retrofit and database dependencies that will now be provided by dagger

--- app/src/main/java/com/seanPeters/newsapp/data/NewsArticleDatabase.kt
+++ app/src/main/java/com/seanPeters/newsapp/data/NewsArticleDatabase.kt
@@ -1,9 +1,11 @@
 package com.seanPeters.newsapp.data
 
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 @Database(entities = [NewsArticle::class,BreakingNews::class],version = 1)
-abstract class NewsArticleDatabase {
+abstract class NewsArticleDatabase:RoomDatabase() {
 
     abstract fun newsArticleDao():NewsArticleDao
 }
---
+++ app/src/main/java/com/seanPeters/newsapp/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.seanPeters.newsapp.di
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.seanPeters.newsapp.api.NewsApi
+import com.seanPeters.newsapp.data.NewsArticleDatabase
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+object AppModule {
+    @Provides
+    @Singleton
+    fun providesRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(NewsApi.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(retrofit: Retrofit):NewsApi =
+        retrofit.create(NewsApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providesDatabase(app:Application):NewsArticleDatabase =
+        Room.databaseBuilder(app,NewsArticleDatabase::class.java)
+            .fallbackToDestructiveMigration()
+            .build()
+
+
+
+}
